70198d61f012e6217a2f74d9ad5e27d35203fc4f
Stanislau Vyrski <vyr4376@yandex.ru>
2026-02-26 16:12:36 +0300

    feat: adaptive implemented for about

--- src/pages/About/AboutPage.jsx
+++ src/pages/About/AboutPage.jsx
@@ -9,24 +9,24 @@ export default function AboutPage() {
   return (
     <section className={styles.about}>
       <Wrapper className={styles.wrapper}>
-        <Text className={styles.title} variant="h1">
+        <Text className={styles.title} tag="h1" variant="big">
           About Our Store
         </Text>
         <div className={styles["content-wrapper"]}>
           <div className={styles["description-wrapper"]}>
-            <Text className={styles.description}>
+            <Text className={styles.description} variant="basic-text">
               Welcome to our online store, your go-to destination for
               high-quality beauty, personal care, and lifestyle products. From
               top-brand cosmetics like Essence Mascara Lash Princess to
               carefully selected everyday essentials, we bring you products that
               are loved by our customers worldwide.
             </Text>
-            <Text className={styles.description}>
+            <Text className={styles.description} variant="basic-text">
               Every item in our collection is chosen for quality, functionality,
               and style. We provide detailed product information, customer
               reviews, and real-time availability so you can shop confidently.
             </Text>
-            <Text className={styles.description}>
+            <Text className={styles.description} variant="basic-text">
               Enjoy great deals, clear shipping details, and a hassle-free
               return policy. Our mission is to make your shopping experience
               smooth, enjoyable, and rewarding. Discover our exclusive selection
@@ -35,8 +35,8 @@ export default function AboutPage() {
           </div>
           <img
             className={styles["customer-img"]}
-            width={550}
-            height={395}
+            width={500}
+            height={359}
             src={customerImg}
             alt="Happy customer shopping beauty products"
           />
